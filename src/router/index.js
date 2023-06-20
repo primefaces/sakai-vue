7cc3b51281a41577e278918821508437c3436093
arismoratalla <arismoratalla@gmail.com>
2023-06-20 21:11:36 +0800

    initial push

--- src/router/index.js
+++ src/router/index.js
@@ -10,8 +10,8 @@ const router = createRouter({
             children: [
                 {
                     path: '/',
-                    name: 'dashboard',
-                    component: () => import('@/views/Dashboard.vue')
+                    name: 'portal',
+                    component: () => import('@/views/Portal.vue')
                 },
                 {
                     path: '/uikit/formlayout',
---
+++ src/views/Portal.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <BlockViewer header="Feature">
+            <div class="surface-section px-4 py-8 md:px-6 lg:px-8 text-center">
+                <div class="mb-3 font-bold text-2xl">
+                    <span class="text-900">Welcome to the DOST-IX, </span>
+                    <span class="text-blue-600">Staging Portal</span>
+                </div>
+                <div class="text-700 text-sm mb-6">This is the place where ideas come to life and projects take shape before they are launched into the world. With our staging server, you can explore and test your applications in a controlled environment, ensuring smooth functionality and optimal performance before going live. Whether you're a developer, a designer, or a project manager, this server provides a collaborative space for you to fine-tune your creations, gather feedback, and iterate until perfection is achieved. Embrace the power of experimentation and innovation as you embark on your journey towards excellence. Get ready to unleash your creativity and witness your ideas flourish on our secure and reliable staging server. Let's begin this exciting adventure together!</div>
+                <div class="grid">
+                    <div class="col-12 md:col-4 mb-4 px-5">
+                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
+                            <i class="pi pi-desktop text-4xl text-blue-500"></i>
+                        </span>
+                        <div class="text-900 mb-3 font-medium">FAIMS</div>
+                        <span class="text-700 text-sm line-height-3">Finance and Adminisrative Information Management System</span>
+                    </div>
+                    <div class="col-12 md:col-4 mb-4 px-5">
+                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
+                            <i class="pi pi-lock text-4xl text-blue-500"></i>
+                        </span>
+                        <div class="text-900 mb-3 font-medium">eULIMS</div>
+                        <span class="text-700 text-sm line-height-3">Enhanced Unified Laboratory Information Management System</span>
+                    </div>
+                    <div class="col-12 md:col-4 mb-4 px-5">
+                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
+                            <i class="pi pi-check-circle text-4xl text-blue-500"></i>
+                        </span>
+                        <div class="text-900 mb-3 font-medium">DMS</div>
+                        <span class="text-700 text-sm line-height-3">Document Management System</span>
+                    </div>
+                    <div class="col-12 md:col-4 mb-4 px-5">
+                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
+                            <i class="pi pi-globe text-4xl text-blue-500"></i>
+                        </span>
+                        <div class="text-900 mb-3 font-medium">TRACE</div>
+                        <span class="text-700 text-sm line-height-3">Tracking, Retrieval, Archiving of Communications for Efficiency</span>
+                    </div>
+                    <div class="col-12 md:col-4 mb-4 px-5">
+                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
+                            <i class="pi pi-github text-4xl text-blue-500"></i>
+                        </span>
+                        <div class="text-900 mb-3 font-medium">VRAMS</div>
+                        <span class="text-700 text-sm line-height-3">Online Vehicle Reservation and Management. Also facilitates filing of Travel Orders within the Region.</span>
+                    </div>
+                    <div class="col-12 md:col-4 md:mb-4 mb-0 px-3">
+                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
+                            <i class="pi pi-shield text-4xl text-blue-500"></i>
+                        </span>
+                        <div class="text-900 mb-3 font-medium">SCIMS</div>
+                        <span class="text-700 text-sm line-height-3">Records the withdrawal of items made by end-user and receiving transactions from suppliers. The system aids the supply and accounting unit on generating reports as required by the office.</span>
+                    </div>
+                </div>
+            </div>
+        </BlockViewer>
+    </div>
+</template>

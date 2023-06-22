e5026f09b64555652f4bd95c239c7cca4058296c
arismoratalla <arismoratalla@gmail.com>
2023-06-22 13:23:50 +0800

    added more InfoSys

--- src/views/Portal.vue
+++ src/views/Portal.vue
@@ -1,61 +1,89 @@
 <template>
     <div>
-        <BlockViewer header="Feature">
-            <div class="surface-section px-4 py-8 md:px-6 lg:px-8 text-center">
+        <BlockViewer header="">
+            <div class="surface-section px-4 py-4 md:px-6 lg:px-4 text-center">
                 <div class="mb-3 font-bold text-2xl">
                     <span class="text-900">Welcome to the DOST-IX </span>
-                    <span class="text-blue-600">Staging Portal</span>
+                    <span class="text-blue-600">Web Portal</span>
                 </div>
-                <div class="text-700 text-sm mb-6">
-                    This is the place where ideas come to life and projects take shape before they are launched into the world. With our staging server, you can explore and test your applications in a controlled environment, ensuring smooth
-                    functionality and optimal performance before going live. Whether you're a developer, a designer, or a project manager, this server provides a collaborative space for you to fine-tune your creations, gather feedback, and iterate
-                    until perfection is achieved. Embrace the power of experimentation and innovation as you embark on your journey towards excellence. Get ready to unleash your creativity and witness your ideas flourish on our secure and reliable
-                    staging server. Let's begin this exciting adventure together!
+                <div class="text-700 text-md mb-6">
+                    This portal serves as an online directory of web applications currently deployed to DOST-IX. <br />
+                    Select the web application you wish to access. (some applications may require login credentials - contact your system administrator if you don't have one).
                 </div>
                 <div class="grid">
                     <div class="col-12 md:col-4 mb-4 px-5">
                         <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
-                            <i class="pi pi-desktop text-4xl text-blue-500"></i>
-                        </span>
-                        <div class="text-900 mb-3 font-medium">FAIMS</div>
-                        <span class="text-700 text-sm line-height-3">Finance and Adminisrative Information Management System</span>
-                    </div>
-                    <div class="col-12 md:col-4 mb-4 px-5">
-                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
-                            <i class="pi pi-lock text-4xl text-blue-500"></i>
-                        </span>
-                        <div class="text-900 mb-3 font-medium">eULIMS</div>
-                        <span class="text-700 text-sm line-height-3">Enhanced Unified Laboratory Information Management System</span>
-                    </div>
-                    <div class="col-12 md:col-4 mb-4 px-5">
-                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
-                            <i class="pi pi-check-circle text-4xl text-blue-500"></i>
+                            <i class="pi pi-folder-open text-4xl text-blue-500"></i>
                         </span>
                         <div class="text-900 mb-3 font-medium">DMS</div>
                         <span class="text-700 text-sm line-height-3">Document Management System</span>
                     </div>
                     <div class="col-12 md:col-4 mb-4 px-5">
                         <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
-                            <i class="pi pi-globe text-4xl text-blue-500"></i>
+                            <i class="pi pi-check-square text-4xl text-blue-500"></i>
+                        </span>
+                        <div class="text-900 mb-3 font-medium">eULIMS</div>
+                        <span class="text-700 text-sm line-height-3">Enhanced Unified Laboratory Information Management System</span>
+                    </div>
+                    <div class="col-12 md:col-4 mb-4 px-5">
+                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
+                            <i class="pi pi-book text-4xl text-blue-500"></i>
+                        </span>
+                        <div class="text-900 mb-3 font-medium">FAIMS</div>
+                        <span class="text-700 text-sm line-height-3">Finance and Administrative Information Management System</span>
+                    </div>
+                    <div class="col-12 md:col-4 mb-4 px-5">
+                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
+                            <i class="pi pi-server text-4xl text-blue-500"></i>
+                        </span>
+                        <div class="text-900 mb-3 font-medium">File Server Online</div>
+                        <span class="text-700 text-sm line-height-3">Online repository of DOST-IX files and documents.</span>
+                    </div>
+                    <div class="col-12 md:col-4 mb-4 px-5">
+                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
+                            <i class="pi pi-user text-4xl text-blue-500"></i>
+                        </span>
+                        <div class="text-900 mb-3 font-medium">HRMIS</div>
+                        <span class="text-700 text-sm line-height-3">Human Resource Management Information System</span>
+                    </div>
+                    <div class="col-12 md:col-4 mb-4 px-5">
+                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
+                            <i class="pi pi-map text-4xl text-blue-500"></i>
+                        </span>
+                        <div class="text-900 mb-3 font-medium">IMPRESSION</div>
+                        <span class="text-700 text-sm line-height-3">Information & Monitoring of Projects, Services and S&T Interventions.</span>
+                    </div>
+                    <div class="col-12 md:col-4 md:mb-4 mb-0 px-3">
+                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
+                            <i class="pi pi-shopping-cart text-4xl text-blue-500"></i>
+                        </span>
+                        <div class="text-900 mb-3 font-medium">SCIMS</div>
+                        <span class="text-700 text-sm line-height-3">
+                            Records the withdrawal of items made by end-user and receiving transactions from suppliers. The system aids the supply and accounting unit on generating reports as required by the office.</span
+                        >
+                    </div>
+                    <div class="col-12 md:col-4 md:mb-4 mb-0 px-3">
+                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
+                            <i class="pi pi-star text-4xl text-blue-500"></i>
+                        </span>
+                        <div class="text-900 mb-3 font-medium">SCHOLARSHIP</div>
+                        <span class="text-700 text-sm line-height-3">
+                            An information system used for monitoring and evaluation of DOST scholar's performance. Contains basic information and financial benefits claimed/received of DOST-scholars enrolled in the region. The system also tracks the wherabouts of DOST-scholar graduates and is also capable of generating Financial Reports and Status reports as required by the office.
+                        </span>
+                    </div>
+                    <div class="col-12 md:col-4 mb-4 px-5">
+                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
+                            <i class="pi pi-file-export text-4xl text-blue-500"></i>
                         </span>
                         <div class="text-900 mb-3 font-medium">TRACE</div>
                         <span class="text-700 text-sm line-height-3">Tracking, Retrieval, Archiving of Communications for Efficiency</span>
                     </div>
                     <div class="col-12 md:col-4 mb-4 px-5">
                         <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
-                            <i class="pi pi-github text-4xl text-blue-500"></i>
+                            <i class="pi pi-car text-4xl text-blue-500"></i>
                         </span>
                         <div class="text-900 mb-3 font-medium">VRAMS</div>
                         <span class="text-700 text-sm line-height-3">Online Vehicle Reservation and Management. Also facilitates filing of Travel Orders within the Region.</span>
-                    </div>
-                    <div class="col-12 md:col-4 md:mb-4 mb-0 px-3">
-                        <span class="p-3 shadow-2 mb-3 inline-block surface-card" style="border-radius: 10px">
-                            <i class="pi pi-shield text-4xl text-blue-500"></i>
-                        </span>
-                        <div class="text-900 mb-3 font-medium">SCIMS</div>
-                        <span class="text-700 text-sm line-height-3"
-                            >Records the withdrawal of items made by end-user and receiving transactions from suppliers. The system aids the supply and accounting unit on generating reports as required by the office.</span
-                        >
                     </div>
                 </div>
             </div>

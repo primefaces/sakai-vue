0a9ebf77b74d184d2374574068e69fdc22df7ec3
arismoratalla <arismoratalla@gmail.com>
2023-06-22 09:08:33 +0800

    specified build outDir

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig(() => {
                 '@': fileURLToPath(new URL('./src', import.meta.url)),
             },
         },
+        build: {
+            outDir: './staging',
+        },
         server: {
             host: true,
         },

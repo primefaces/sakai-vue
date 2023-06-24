419eb5becca508bef4c04acc3b3e896a00738692
Victor Noé  Becerra Hernández <victor.becerra@aiscs.com.mx>
2023-06-24 15:09:58 -0600

    Update AppFooter.vue

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -11,9 +11,7 @@ const logoUrl = computed(() => {
 
 <template>
     <div class="layout-footer">
-        <img :src="logoUrl" alt="Logo" height="20" class="mr-2" />
-        by
-        <span class="font-medium ml-2">PrimeVue</span>
+        2023
     </div>
 </template>
 <style lang="scss" scoped></style>

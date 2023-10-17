a9e61fab32abd0df9288095828fbb0ad48a33598
Moises Serrano <moycs777@gmail.com>
2023-10-17 11:57:35 -0300

    [FEAT] Client detail, account settings

--- src/views/clients/components/ClientDetailTabs.vue
+++ src/views/clients/components/ClientDetailTabs.vue
@@ -1,134 +1,74 @@
 <script setup lang="ts">
 import TabView from 'primevue/tabview';
 import TabPanel from 'primevue/tabpanel';
-import InputSwitch from 'primevue/inputswitch';
+import ClientPersonalDataTab from '@/views/clients/components/ClientPersonalDataTab.vue';
 import { ref } from 'vue';
-import Dialog from 'primevue/dialog';
+import Slider from 'primevue/slider';
+import InputSwitch from 'primevue/inputswitch';
 
-const isReceivingSMS = ref(false);
-const isKYCVisible = ref(false);
-const isPhotosVisible = ref(false);
+const swapFeeAsset = ref(50);
+const swapFeeFiat = ref(50);
+const isSwapEnable = ref(false);
 </script>
 
 <template>
   <div class="col-12">
     <div class="card">
-      <!-- KYC -->
-      <Dialog v-model:visible="isKYCVisible" modal header="Información de la cuenta" :style="{ width: '50vw' }">
-        <p>CIP/AML</p>
-        <p>KYC ACTIONS</p>
-
-        <template #footer>
-          <Button label="Cerrar" @click="isKYCVisible = false" />
-        </template>
-      </Dialog>
-
-      <!--  Fotos -->
-      <Dialog v-model:visible="isPhotosVisible" modal header="Imagenes de los documentos" :style="{ width: '50vw' }">
-        <p></p>
-
-        <template #footer>
-          <Button label="Cerrar" @click="isPhotosVisible = false" />
-        </template>
-      </Dialog>
-
       <TabView>
         <TabPanel header="Datos del cliente">
-          <div class="p-fluid formgrid grid">
-            <div class="field col-12 md:col-8">
-              <h4>Datos personales</h4>
-            </div>
-            <div class="field col-12 md:col-2">
-              <Button @click="isKYCVisible = true" label="KYC" outlined size="small" />
-            </div>
-            <div class="field col-12 md:col-2">
-              <Button @click="isPhotosVisible = true" label="FOTOS" outlined size="small" />
-            </div>
-
-            <div class="field col-12 md:col-2">
-              <label for="state">ID: </label>
-              <p>data</p>
-            </div>
-
-            <div class="field col-12 md:col-2">
-              <label for="state">Nombre: </label>
-              <p>data</p>
-            </div>
-
-            <div class="field col-12 md:col-2">
-              <label for="state">Nro documento: </label>
-              <p>data</p>
-            </div>
-
-            <div class="field col-12 md:col-2">
-              <label for="state">Pais del documento: </label>
-              <p>data</p>
-            </div>
-
-            <div class="field col-12 md:col-2">
-              <label for="state">Nro telefono: </label>
-              <p>data</p>
-            </div>
-
-            <div class="field col-12 md:col-2">
-              <label for="state">recibe sms: </label>
-              <InputSwitch disabled v-model="isReceivingSMS" />
-            </div>
-
-            <div class="field col-12 md:col-2">
-              <label for="state">Email: </label>
-              <p>data</p>
-            </div>
-            <!--            -->
-
-            <div class="field col-12 md:col-12">
-              <h4>Domicilio fiscal</h4>
-            </div>
-
-            <div class="field col-12 md:col-2">
-              <label for="state">Pais: </label>
-              <p>data</p>
-            </div>
-
-            <div class="field col-12 md:col-2">
-              <label for="state">Estado: </label>
-              <p>data</p>
-            </div>
-
-            <div class="field col-12 md:col-2">
-              <label for="state">Ciudad: </label>
-              <p>data</p>
-            </div>
-
-            <div class="field col-12 md:col-2">
-              <label for="state">Direccion 1: </label>
-              <p>data</p>
-            </div>
-
-            <div class="field col-12 md:col-2">
-              <label for="state">Direccion 2: </label>
-              <p>data</p>
-            </div>
-
-            <div class="field col-12 md:col-2">
-              <label for="state">Codigo postal: </label>
-              <p>data</p>
-            </div>
-          </div>
+          <ClientPersonalDataTab></ClientPersonalDataTab>
         </TabPanel>
 
         <TabPanel header="Socios">
-          <p class="line-height-3 m-0">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-            voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
-          </p>
+          <!--          todo -->
+          <!--    iterate each partner      -->
+          <ClientPersonalDataTab></ClientPersonalDataTab>
+          <ClientPersonalDataTab></ClientPersonalDataTab>
         </TabPanel>
 
         <TabPanel header="Configuracion de la cuenta">
-          <p class="line-height-3 m-0">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia
-            deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
-          </p>
+          <div class="p-fluid formgrid grid">
+            <div class="field col-12 md:col-12">
+              <h4>Configuracion de fee</h4>
+            </div>
+
+            <div class="field col-12 md:col-2">
+              <label for="state">Wire domestico (IN): </label>
+              <InputText type="text" placeholder=""></InputText>
+            </div>
+
+            <div class="field col-12 md:col-2">
+              <label for="state">Wire domestico (OUT): </label>
+              <InputText type="text" placeholder=""></InputText>
+            </div>
+
+            <div class="field col-12 md:col-2">
+              <label for="state">Wire Internacional (IN): </label>
+              <InputText type="text" placeholder=""></InputText>
+            </div>
+
+            <div class="field col-12 md:col-2">
+              <label for="state">Wire Internacional (OUT): </label>
+              <InputText type="text" placeholder=""></InputText>
+            </div>
+
+            <div class="field col-12 md:col-2">
+              <label for="state">Fee Swap Fiat - Asset: </label>
+              <InputText :value="swapFeeAsset" disabled />
+              <Slider v-model="swapFeeAsset" />
+            </div>
+
+            <div class="field col-12 md:col-2">
+              <label for="state">Fee Swap Asset - Fiat : </label>
+              <InputText :value="swapFeeFiat" disabled />
+              <Slider v-model="swapFeeFiat" />
+            </div>
+
+            <div class="field col-12 md:col-2">
+              <label for="state">Módulo de swap habilitado?: </label>
+              <InputSwitch disabled v-model="isSwapEnable" />
+            </div>
+          </div>
         </TabPanel>
       </TabView>
     </div>
---
+++ src/views/clients/components/ClientPersonalDataTab.vue
@@ -0,0 +1,112 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Dialog from 'primevue/dialog';
+import InputSwitch from 'primevue/inputswitch';
+
+const isReceivingSMS = ref(false);
+const isKYCVisible = ref(false);
+const isPhotosVisible = ref(false);
+</script>
+
+<template>
+  <div class="p-fluid formgrid grid">
+    <!-- KYC -->
+    <Dialog v-model:visible="isKYCVisible" modal header="Información de la cuenta" :style="{ width: '50vw' }">
+      <p>CIP/AML</p>
+      <p>KYC ACTIONS</p>
+
+      <template #footer>
+        <Button label="Cerrar" @click="isKYCVisible = false" />
+      </template>
+    </Dialog>
+
+    <!--  Fotos -->
+    <Dialog v-model:visible="isPhotosVisible" modal header="Imagenes de los documentos" :style="{ width: '50vw' }">
+      <p></p>
+
+      <template #footer>
+        <Button label="Cerrar" @click="isPhotosVisible = false" />
+      </template>
+    </Dialog>
+
+    <div class="field col-12 md:col-8">
+      <h4>Datos personales</h4>
+    </div>
+    <div class="field col-12 md:col-2">
+      <Button @click="isKYCVisible = true" label="KYC" outlined size="small" />
+    </div>
+    <div class="field col-12 md:col-2">
+      <Button @click="isPhotosVisible = true" label="FOTOS" outlined size="small" />
+    </div>
+
+    <div class="field col-12 md:col-2">
+      <label for="state">ID: </label>
+      <p>data</p>
+    </div>
+
+    <div class="field col-12 md:col-2">
+      <label for="state">Nombre: </label>
+      <p>data</p>
+    </div>
+
+    <div class="field col-12 md:col-2">
+      <label for="state">Nro documento: </label>
+      <p>data</p>
+    </div>
+
+    <div class="field col-12 md:col-2">
+      <label for="state">Pais del documento: </label>
+      <p>data</p>
+    </div>
+
+    <div class="field col-12 md:col-2">
+      <label for="state">Nro telefono: </label>
+      <p>data</p>
+    </div>
+
+    <div class="field col-12 md:col-2">
+      <label for="state">recibe sms: </label>
+      <InputSwitch disabled v-model="isReceivingSMS" />
+    </div>
+
+    <div class="field col-12 md:col-2">
+      <label for="state">Email: </label>
+      <p>data</p>
+    </div>
+    <!--            -->
+
+    <div class="field col-12 md:col-12">
+      <h4>Domicilio fiscal</h4>
+    </div>
+
+    <div class="field col-12 md:col-2">
+      <label for="state">Pais: </label>
+      <p>data</p>
+    </div>
+
+    <div class="field col-12 md:col-2">
+      <label for="state">Estado: </label>
+      <p>data</p>
+    </div>
+
+    <div class="field col-12 md:col-2">
+      <label for="state">Ciudad: </label>
+      <p>data</p>
+    </div>
+
+    <div class="field col-12 md:col-2">
+      <label for="state">Direccion 1: </label>
+      <p>data</p>
+    </div>
+
+    <div class="field col-12 md:col-2">
+      <label for="state">Direccion 2: </label>
+      <p>data</p>
+    </div>
+
+    <div class="field col-12 md:col-2">
+      <label for="state">Codigo postal: </label>
+      <p>data</p>
+    </div>
+  </div>
+</template>

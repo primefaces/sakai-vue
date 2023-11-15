d07a0689e0e5ff98c67b21054b73a65d7483ff32
Oscar-gg <oscar.arreola.jr@gmail.com>
2023-11-15 14:12:59 -0600

    feature: example table from primevue

--- src/views/Horarios.vue
+++ src/views/Horarios.vue
@@ -1,9 +1,158 @@
-<script setup>
 
+<script setup>
+import { ref, onMounted } from 'vue';
+import { FilterMatchMode } from 'primevue/api';
+import { CustomerService } from '@/service/CustomerService';
+
+const customers = ref();
+const filters = ref({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    'country.name': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    representative: { value: null, matchMode: FilterMatchMode.IN },
+    status: { value: null, matchMode: FilterMatchMode.EQUALS },
+    verified: { value: null, matchMode: FilterMatchMode.EQUALS }
+});
+const representatives = ref([
+    { name: 'Amy Elsner', image: 'amyelsner.png' },
+    { name: 'Anna Fali', image: 'annafali.png' },
+    { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
+    { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
+    { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
+    { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
+    { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
+    { name: 'Onyama Limba', image: 'onyamalimba.png' },
+    { name: 'Stephen Shaw', image: 'stephenshaw.png' },
+    { name: 'XuXue Feng', image: 'xuxuefeng.png' }
+]);
+const statuses = ref(['unqualified', 'qualified', 'new', 'negotiation', 'renewal', 'proposal']);
+const loading = ref(true);
+
+onMounted(() => {
+    CustomerService.getCustomersMedium().then((data) => {
+        customers.value = getCustomers(data);
+        loading.value = false;
+    });
+});
+
+const getCustomers = (data) => {
+    return [...(data || [])].map((d) => {
+        d.date = new Date(d.date);
+
+        return d;
+    });
+};
+const formatDate = (value) => {
+    return value.toLocaleDateString('en-US', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    });
+};
+const formatCurrency = (value) => {
+    return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+};
+const getSeverity = (status) => {
+    switch (status) {
+        case 'unqualified':
+            return 'danger';
+
+        case 'qualified':
+            return 'success';
+
+        case 'new':
+            return 'info';
+
+        case 'negotiation':
+            return 'warning';
+
+        case 'renewal':
+            return null;
+    }
+}
 </script>
 
+
 <template>
-    <div class="flex align-content-center h-full" style="min-height: 300px">
-        <h1 class="flex align-items-center justify-content-center w-full">Horarios: !En construcción! ✌</h1>
+    <div class="flex flex-col flex-wrap">
+
+        <h1 class="w-full text-blue-800 h-fit">Horario</h1>
+
+        <div class="card">
+            <DataTable v-model:filters="filters" :value="customers" paginator :rows="10" dataKey="id" filterDisplay="row"
+                :loading="loading" :globalFilterFields="['name', 'country.name', 'representative.name', 'status']">
+                <template #header>
+                    <div class="flex justify-content-end">
+                        <span class="p-input-icon-left">
+                            <i class="pi pi-search" />
+                            <InputText v-model="filters['global'].value" placeholder="Keyword Search" />
+                        </span>
+                    </div>
+                </template>
+                <template #empty> No se encontraron Maes. </template>
+                <template #loading> Cargando información. Por favor espera.</template>
+                <Column header="Nombre" field="name"  style="min-width: 12rem">
+                    <template #body="{ data }">
+                        {{ data.name }}
+                    </template>
+                    <template #filter="{ filterModel, filterCallback }">
+                        <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter"
+                            placeholder="Busca por nombre" />
+                    </template>
+                </Column>
+                <Column header="Materias" filterField="country.name" style="min-width: 12rem">
+                    <template #body="{ data }">
+                        <div class="flex align-items-center gap-2">
+                            <img alt="flag" src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
+                                :class="`flag flag-mx`" style="width: 24px" />
+                            <span>{{ data.country.name }}</span>
+                        </div>
+                    </template>
+                    <template #filter="{ filterModel, filterCallback }">
+                        <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter"
+                            placeholder="Busca por materia" />
+                    </template>
+                </Column>
+                <Column header="Horario" filterField="representative" :showFilterMenu="false"
+                    :filterMenuStyle="{ width: '14rem' }" style="min-width: 14rem">
+                    <template #body="{ data }">
+                        <div class="flex align-items-center gap-2">
+                            <img :alt="data.representative.name"
+                                :src="`https://primefaces.org/cdn/primevue/images/avatar/${data.representative.image}`"
+                                style="width: 32px" />
+                            <span>{{ data.representative.name }}</span>
+                        </div>
+                    </template>
+                    <template #filter="{ filterModel, filterCallback }">
+                        <MultiSelect v-model="filterModel.value" @change="filterCallback()" :options="representatives"
+                            optionLabel="name" placeholder="Cualquiera" class="p-column-filter" style="min-width: 14rem"
+                            :maxSelectedLabels="1">
+                            <template #option="slotProps">
+                                <div class="flex align-items-center gap-2">
+                                    <img :alt="slotProps.option.name"
+                                        :src="`https://primefaces.org/cdn/primevue/images/avatar/${slotProps.option.image}`"
+                                        style="width: 32px" />
+                                    <span>{{ slotProps.option.name }}</span>
+                                </div>
+                            </template>
+                        </MultiSelect>
+                    </template>
+                </Column>
+                <Column field="status" header="Modalidad" :showFilterMenu="false" :filterMenuStyle="{ width: '14rem' }"
+                    style="min-width: 12rem">
+                    <template #body="{ data }">
+                        <Tag :value="data.status" :severity="getSeverity(data.status)" />
+                    </template>
+                    <template #filter="{ filterModel, filterCallback }">
+                        <Dropdown v-model="filterModel.value" @change="filterCallback()" :options="statuses"
+                            placeholder="Cualquiera" class="p-column-filter" style="min-width: 12rem" :showClear="true">
+                            <template #option="slotProps">
+                                <Tag :value="slotProps.option" :severity="getSeverity(slotProps.option)" />
+                            </template>
+                        </Dropdown>
+                    </template>
+                </Column>
+            </DataTable>
+        </div>
     </div>
 </template>

10246ff426d43a7485efcb4e0c75e1d140d87468
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-11-16 13:24:38 -0600

    Pantalla terminada
    La pantalla esta semi terminada y esta sujeta a diseños

---
+++ src/components/MaeCard.vue
@@ -0,0 +1,32 @@
+<script setup>
+// Props para recibir del padre
+const props = defineProps(['nombre', 'carrera', 'matricula', 'horaInicio', 'horaFin']);
+console.log(props);
+</script>
+
+<template>
+    <div class="relative sm:w-30% md:w-30% lg:w-30% shrink-0 text-black ml-3 mt-5 mb-5" style="border-radius: 15px; overflow: hidden; border: 1px solid #ccc">
+        <div class="flex" style="background-color: #09144f; border-radius: 25px 25px 0 0">
+            <div class="pl-2 p-2">
+                <p class="text-4xl font-semibold text-white">{{ nombre }}</p>
+            </div>
+            <div class="ml-auto pr-4 p-2">
+                <p class="text-4xl font-semibold text-white text-end">{{ carrera }}</p>
+            </div>
+        </div>
+
+        <div class="flex mt-5">
+            <div class="flex top-1/2 transform -translate-y-1/2 bg-gray-300 mb-5" style="width: 80px; height: 80px; border-radius: 50%; margin-left: 1%"></div>
+            <b class="flex text-4xl text-blue-800" style="padding-left: 8%; padding-right: 20%">{{ matricula }}</b>
+            <div class="flex top-1/2 transform -translate-y-1/2 bg-gray-300" style="width: 25px; height: 25px; border-radius: 50%"></div>
+            <div class="flex" style="margin-left: -60%; margin-top: 10%">
+                <div class="bg-green-200 text-2xl flex items-center justify-center text-center px-4" style="border-radius: 25%; height: 35px; margin-right: 50%">
+                    <p class="text-gray-500">{{ horaInicio }}</p>
+                </div>
+                <div class="bg-green-500 text-2xl flex items-center justify-center text-center px-4" style="border-radius: 25%; height: 35px">
+                    <p class="text-white">{{ horaFin }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/views/Coordi.vue
+++ src/views/Coordi.vue
@@ -1,49 +1,45 @@
 <script setup>
 import Button from 'primevue/button';
+import MaeCard from '../components/MaeCard.vue';
+import { computed } from 'vue';
+
+const maesData = [
+    { nombre: 'Juan Perez', carrera: 'ITC', matricula: 'A01733822', horaInicio: '11:00', horaFin: '12:00', activo: true },
+    { nombre: 'María Rodriguez', carrera: 'ELE', matricula: 'A01733823', horaInicio: '10:00', horaFin: '11:30', activo: true },
+    { nombre: 'Genoveva Flores', carrera: 'ELE', matricula: 'A01733823', horaInicio: '10:00', horaFin: '11:30', activo: true },
+    { nombre: 'Maria Garza', carrera: 'ELE', matricula: 'A01733823', horaInicio: '10:00', horaFin: '11:30', activo: true },
+    { nombre: 'Carlos Gonzalez', carrera: 'MAT', matricula: 'A01733824', horaInicio: '09:30', horaFin: '10:45', activo: false },
+    { nombre: 'Angela Gonzalez', carrera: 'IRS', matricula: 'A01733824', horaInicio: '12:30', horaFin: '13:30', activo: false },
+    { nombre: 'Fernando Lopez', carrera: 'IRS', matricula: 'A01733824', horaInicio: '12:30', horaFin: '13:30', activo: false },
+    { nombre: 'Nepomuceno Vargas', carrera: 'IRS', matricula: 'A01733824', horaInicio: '12:30', horaFin: '13:30', activo: false }
+    // Agrega más objetos según sea necesario
+];
+// Propiedad computada para filtrar maesData
+const maesActivos = computed(() => maesData.filter((mae) => mae.activo));
+const maesPendientes = computed(() => maesData.filter((mae) => !mae.activo));
 </script>
 
 <template>
-    <div class="rounded-3xl bg-white overflow-hidden text-left text-2xl text-blue-800 font-lato p-5 w-screen h-screen px-3">
-        <div class="flex relative">
+    <div class="bg-white overflow-hidden font-lato px-4">
+        <div class="flex relative mt-5">
             <div class="flex-1">
-                <div class="text-black text-4xl mb-5 ml-2.4%">Coordinación</div>
+                <div class="text-black text-6xl mb-5 ml-2.4%">Coordinación</div>
                 <div class="border-b-2 border-d6d6d6 w-full mb-5 ml-2.4%"></div>
             </div>
-            <div class="flex-2">
-                <div class="flex">
-                    <Button label="Generar Reporte" severity="info" size="large" />
-                </div>
+
+            <div class="justify-end">
+                <Button label="Generar Reporte" severity="info" size="large" class="ml-5" />
             </div>
         </div>
         <div class="mt-5">
-            <div class="text-3xl"><b>Maes Activos</b></div>
-            <div class="flex mt-5">
-                <div class="relative rounded-l sm:w-30% md:w-30% lg:w-30% shrink-0 text-black ml-5 mr-5 mt-5">
-                    <div class="flex" style="background-color: #09144f">
-                        <div class="pl-6 p-2">
-                            <p class="text-4xl font-semibold text-white">Juan Perez</p>
-                        </div>
-                        <div class="ml-auto pr-3 p-2">
-                            <p class="text-4xl font-semibold text-white text-end">ITC</p>
-                        </div>
-                    </div>
-
-                    <div class="flex mt-5">
-                        <div class="relative top-1/2 transform -translate-y-1/2 bg-gray-300" style="width: 100px; height: 100px; border-radius: 100%; margin-left: 1%"></div>
-                        <b class="relative text-4xl" style="padding-left: 8%; padding-right: 20%">A01733822</b>
-                        <div class="relative top-1/2 transform -translate-y-1/2 bg-gray-300" style="width: 30px; height: 30px; border-radius: 100%"></div>
-                        <div class="flex" style="margin-left: -58%; margin-top: 10%">
-                            <div class="bg-green-200 mr-10 text-4xl" style="width: 100px; height: 40px; border-radius: 30%; margin-right: 76%">
-                                <p class="text-center text-black">11:00</p>
-                            </div>
-                            <div class="bg-green-500 text-4xl" style="width: 100px; height: 40px; border-radius: 30%">
-                                <p class="text-center text-black">12:00</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="text-5xl"><b>Maes Activos</b></div>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
+                <MaeCard v-for="mae in maesActivos" :key="mae.matricula" :nombre="mae.nombre" :carrera="mae.carrera" :matricula="mae.matricula" :horaInicio="mae.horaInicio" :horaFin="mae.horaFin" />
             </div>
-            <div class="text-3xl mt-2.4% pt-5"><b>Maes Pendientes</b></div>
+            <div class="text-5xl"><b>Maes Pendientes</b></div>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+                <MaeCard v-for="mae in maesPendientes" :key="mae.matricula" :nombre="mae.nombre" :carrera="mae.carrera" :matricula="mae.matricula" :horaInicio="mae.horaInicio" :horaFin="mae.horaFin" />
+            </div>
         </div>
     </div>
 </template>

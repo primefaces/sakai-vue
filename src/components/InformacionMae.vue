36401808bf5e3301a761d8552744bf0c07d9f30c
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2024-01-18 17:31:37 -0600

    Titulo general y tarjeta

---
+++ src/components/InformacionMae.vue
@@ -0,0 +1,10 @@
+
+
+<template>
+     <div class="flex flex-col">
+        <div class="w-full p-0 border-round-xl bg-white shadow-1">
+            <div class="text-black text-4xl">Aserias</div>
+            <div class="text-0D294C text-4xl">22,000 totales</div>
+        </div>
+    </div>
+</template>
--- src/views/AdminAsesorias.vue
+++ src/views/AdminAsesorias.vue
@@ -1,20 +1,34 @@
-<script setup></script>
+<script setup>
+    const fecha = "Martes 14 de Noviembre de 2023"
+</script>
+
 
 <template>
-  <div class="bg-white overflow-hidden font-lato px-4">
+  <div class="overflow-hidden font-lato px-4">
+    
     <div class="flex relative mt-5">
       <div class="flex-1">
-        <div class="text-0D294C text-6xl mb-5 ml-2.4%">Administrador</div>
-      
-        <!-- Línea horizontal con tamaño fijo -->
-        <hr class="w-5 border-t-2 border-D6D6D6 mb-2 ml-2.4%" />
-        <div class="border-b-2 border-d6d6d6 w-full mb-5 ml-2.4%"></div>
+        <div class="text-0D294C text-6xl mb-5 ml-2.4%">Administrador</div> 
       </div>
 
       <div class="justify-end">
-        <Button label="Generar Reporte" severity="info" size="large" class="ml-5 bg-0D294C" />
+        <i class="pi pi-bell text-6xl"></i>
+        <Button label="Generar Reporte" icon="pi pi-file-export" severity="info" size="large" class="ml-5 bg-0D294C" />
       </div>
     </div>
+    <hr class="width-5 border-t-2 border-D6D6D6 mb-2 ml-2.4%" />
+    <div class="border-b-2 border-d6d6d6 w-full mb-5 ml-2.4%"></div>
+    <p class="text-4xl">{{fecha}} </p>
+    <div class="border-b-2 border-d6d6d6 w-full mb-5 ml-2.4%"></div>
+
+    <div class="flex flex-col">
+        <div class="w-full p-0 border-round-xl bg-white shadow-1">
+            <div class="text-black text-4xl">Aserias</div>
+            <div class="text-0D294C text-4xl">22,000 totales</div>
+        </div>
+    </div>
+
+    
   </div>
 </template>
 
@@ -35,27 +49,27 @@
 /* Línea gris */
 .border-D6D6D6 {
   border-color: #D6D6D6;
+  
 }
 
 /* Establece el tamaño fijo de la línea horizontal */
-
-
-/* Ajusta el tamaño de la línea en diferentes tamaños de pantalla */
-@media (max-width: 1600px) {
-  .w-5 {
-    width: 1rem; /* Puedes ajustar este valor según tu preferencia */
-  }
-}
-
-@media (max-width: 976px) {
-  .w-5 {
-    width: 0.75rem; /* Puedes ajustar este valor según tu preferencia */
-  }
+.width-5 {
+  width: 41.6667%;
 }
 
 @media (max-width: 768px) {
-  .w-5 {
-    width: 1rem; /* Puedes ajustar este valor según tu preferencia */
+  .width-5 {
+    width: 55%; /* Puedes ajustar este valor según tu preferencia */
   }
+}
+
+/* Contenedor blanco con borde gris */
+.w-full.border-round-xl.bg-white.shadow-1 {
+  border: 1px solid #D6D6D6;
+}
+
+/* Estilo de los textos internos centrados */
+.text-black, .text-0D294C {
+  text-align: center;
 }
 </style>

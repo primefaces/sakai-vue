8f99fa1e7350e6d65619d232e7894741ac84f409
Rico Guinanao <83864808+coricss@users.noreply.github.com>
2024-02-16 15:02:38 +0800

    chore: fixed AppLayout.vue by returning true when either sidebarEL or topbarEL is not showing

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -53,6 +53,10 @@ const isOutsideClicked = (event) => {
     const sidebarEl = document.querySelector('.layout-sidebar');
     const topbarEl = document.querySelector('.layout-menu-button');
 
+    if (!sidebarEl || !topbarEl) {
+        return true;
+    }
+
     return !(sidebarEl.isSameNode(event.target) || sidebarEl.contains(event.target) || topbarEl.isSameNode(event.target) || topbarEl.contains(event.target));
 };
 </script>

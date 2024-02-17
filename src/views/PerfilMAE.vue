2d2c693997e71ced3a22e0a2bdccb595b27e4030
LuisAGuzmanI <luisangel2002.lagi@gmail.com>
2024-02-17 17:41:04 -0600

    Cambios esteticos: Perfil de MAE

--- src/views/PerfilMAE.vue
+++ src/views/PerfilMAE.vue
@@ -1,147 +1,132 @@
 <script setup>
-import { ref , computed} from 'vue';
-const fecha = "Martes 14 de Noviembre de 2023"
+import { ref, computed } from 'vue';
 const showMoreTags = ref(false);
 const showMoreTags2 = ref(false);
 const materias = [
-  "tc2005b","tc2005b","papa","tc2005b","tc2005b","tc2005b",
-  "tc2005b","tc2005b","tc2005b","tc2005b","tc2005b","tc2005b",
-  "tc2005b","tc2005b","tc2005b","tc2005b","tc2005b","tc2005b",
-  "tc2005b","tc2005b","tc2005b","tc2005b","tc2005b","tc2005b",
+  "tc2005b", "tc2005b", "tc2005b", "tc2005b", "tc2005b",
+  "tc2005b", "tc2005b", "tc2005b", "tc2005b", "tc2005b", "tc2005b",
+  "tc2005b", "tc2005b", "tc2005b", "tc2005b", "tc2005b", "tc2005b",
+  "tc2005b", "tc2005b", "tc2005b", "tc2005b", "tc2005b", "tc2005b",
 ];
+
+const filteredMaterias = computed(() => {
+  return materias.filter(materia => materia.toLowerCase().includes(searchQuery.value.toLowerCase()));
+});
 
 const searchQuery = ref('');
 const horario = [
   ["11:00", "11:00", "11:00", "11:00", "11:00"],
   ["12:00", "12:00", "12:00", "12:00", "12:00"],
-
 ];
-const filteredMaterias = computed(() => {
-  return materias.filter(materia => materia.toLowerCase().includes(searchQuery.value.toLowerCase()));
-});
 
+const showExtraTime = ref(false)
 
-const showDialog = ref(false);
-
-const openDialog = () => {
-  showDialog.value = true;
-};
-
-const closeDialog = () => {
-  showDialog.value = false;
-};
-
-const showDialog2 = ref(false);
-
-const openDialog2 = () => {
-  showDialog2.value = true;
-};
-
-const closeDialog2 = () => {
-  showDialog2.value = false;
-};
+const showDialogHorarios = ref(false);
+const showDialogMaterias = ref(false);
 
 </script>
 
 <template>
-  <div class="overflow-hidden font-lato px-4">
+  <div class="flex relative">
+    <div class="flex-1">
+      <h1 class="text-black text-6xl font-bold mb-5">Perfil</h1>
+    </div>
+    <div class="justify-end hidden md:block">
+      <!-- <i class="pi pi-bell text-4xl"></i> -->
+    </div>
+  </div>
 
-    <div class="flex relative mt-5">
-      <div class="flex-1">
-        <div class="text-0D294C text-6xl  ml-2.4%">Perfil</div>
+  <div class="card mb-0">
+    <div class="flex">
+      <div class="flex flex-1">
+        <img src="https://i.pinimg.com/originals/b1/44/f5/b144f535fb1878b7bdc57aedc14dfc24.jpg" alt="Foto de perfil" class="border-circle h-11rem w-11rem mr-5">
+        <div>
+          <p class="text-3xl font-bold">Luis Ángel Guzmán Iribe</p>
+          <p class="text-lg font-medium"> <i class="pi pi-envelope font-medium"></i> a01741757@tec.mx </p>
+          <p class="text-lg font-medium"> <i class="pi pi-book font-medium"></i> ITC </p>
+          <p class="text-lg font-medium"> <i class="pi pi-building font-medium"></i> Campus MTY </p>
+        </div>
       </div>
-
-      <div class="justify-end">
-        <i class="pi pi-bell text-6xl"></i>
+      <div class="justify-end space-x">
+        <Button label="Materias" icon="pi pi-pencil" size="large"
+          @click="showDialogMaterias = true" />
+        <Button label="Horario" icon="pi pi-pencil" size="large"
+          @click="showDialogHorarios = true" />
       </div>
     </div>
-
-    <hr class="width-5 border-t-2 border-D6D6D6 mb-2 ml-2.4%" />
-    <div class="border-b-2 border-d6d6d6 w-full mb-5 ml-2.4%"></div>
-    <p class="text-4xl">{{fecha}} </p>
-    <div class="border-b-2 border-d6d6d6 w-full mb-5 ml-2.4%"></div>
-    <div class="text-0D294C text-4xl  ml-2.4%">Materias</div>
-
-    <div class="width-3 mb-2" style="margin-top: 20px;">
-      <InputText v-model="searchQuery" placeholder="Buscar materia" class="p-mr-2 w-full" 
-        style="font-size: 1.5rem; background-color: #c2c2c2; color: #000000;::placeholder { color: black; }" />
+    <h2 class="font-bold"> Materias </h2>
+    <div class="mb-2">
+      <InputText v-model="searchQuery" placeholder="Buscar materia" class="p-mr-2 w-full" />
     </div>
     <div class="flex flex-wrap">
       <!-- Mostrar las primeras dos filas de etiquetas -->
-      <div v-for="(materia, index) in filteredMaterias.slice(0, showMoreTags ? Infinity : 12)" :key="index"
-        class="width-2 h-2 p-4">
-        <Tag
-          rounded
-          class="w-full h-4rem text-lg font-semibold bg-0D294C text-white"
-        >
+        <Tag v-for="(materia, index) in filteredMaterias.slice(0, showMoreTags ? Infinity : 12)" :key="index" rounded class="text-lg font-semibold text-white mx-2 my-1 p-3 uppercase">
           {{ materia }}
         </Tag>
-      </div>
-
-      <!-- Mostrar el botón "más/menos" si hay más etiquetas -->
-      <div v-if="materias.length > 12" @click="showMoreTags = !showMoreTags" class="w-full p-2 cursor-pointer ">
-        <div class="flex relative mt-3">
-          <div class="flex-1"></div>
-          <div class="justify-end">
-            <Button
-              :class="{'pi pi-plus-circle': !showMoreTags, 'pi pi-minus-circle': showMoreTags}" 
-               size="large" class="ml-5 bg-0D294C"
-              label="Etiquetas"
-            />
-          </div>
-        </div>
-      </div>
+        <Button v-if="materias.length > 12" @click="showMoreTags = !showMoreTags" :label="showMoreTags ? 'Mostrar menos' : 'Mostrar más'" 
+          severity="secondary" rounded class="text-lg font-semibold text-white mx-2 my-1" :icon="showMoreTags ? 'pi pi-chevron-left' : 'pi pi-chevron-right'" iconPos="right" />
     </div>
 
-    <hr class=" border-t-2 border-D6D6D6 " />
-    <div class="text-0D294C text-4xl  ml-2.4%">Horario</div>
+    <hr />
+    <h2 class="font-bold"> Horario </h2>
 
-    <div class="p-grid">
-      <!-- Días de la semana -->
+    <div>
+      <div class="grid">
+        <div class="col">
+            <div class="text-center p-3 border-round-sm bg-gray-200 text-xl font-bold mb-2">Lunes</div>
+            <div class="text-center p-3 border-round-sm bg-green-500 text-white text-xl font-bold">11:00 - 13:00</div>
+        </div>
+        <div class="col">
+            <div class="text-center p-3 border-round-sm bg-gray-200 text-xl font-bold mb-2">Martes</div>
+            <div class="text-center p-3 border-round-sm bg-green-500 text-white text-xl font-bold ">11:00 - 13:00</div>
+        </div>
+        <div class="col">
+            <div class="text-center p-3 border-round-sm bg-gray-200 text-xl font-bold">Miércoles <Button @click="showExtraTime = !showExtraTime" :icon="showExtraTime ? 'pi pi-chevron-up' : 'pi pi-chevron-down'" class="p-0 text-sm h-1rem w-1rem ml-2"  severity="secondary" text rounded/></div>
+            <div class="text-center p-3 border-round-sm bg-green-500 text-white text-xl font-bold mt-2">11:00 - 13:00</div>
+            <div v-if="showExtraTime" class="text-center p-3 border-round-sm bg-green-500 text-white text-xl font-bold mt-2">11:00 - 13:00</div>
+        </div>
+        <div class="col">
+            <div class="text-center p-3 border-round-sm bg-gray-200 text-xl font-bold mb-2">Jueves</div>
+            <div class="text-center p-3 border-round-sm bg-green-500 text-white text-xl font-bold ">11:00 - 13:00</div>
+        </div>
+        <div class="col">
+            <div class="text-center p-3 border-round-sm bg-gray-200 text-xl font-bold mb-2">Viernes</div>
+            <div class="text-center p-3 border-round-sm bg-green-500 text-white text-xl font-bold ">11:00 - 13:00</div>
+        </div>
+    </div>
+    </div>
+
+    <!-- Días de la semana -->
+    <!-- <div class="p-grid">
       <div class="flex flex-wrap">
         <div v-for="(dia, index) in ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes']" :key="index" class="p-row">
           <div class="text-0D294C text-4xl width-3 h-2 p-5 bg-F4F4F4">{{ dia }}</div>
-
-          <!-- Horas -->
           <div v-for="(hora, rowIndex) in horario" :key="rowIndex" class="width-3 text-4xl p-5">
-            <div
-              :class="{
-                'rounded bg-E2F4E4 text-black p-3 width-3': rowIndex % 2 !== 0,
-                'rounded bg-16591D text-white p-3 width-3': rowIndex % 2 === 0
-              }"
-            >
+            <div :class="{
+              'rounded bg-E2F4E4 text-black p-3 width-3': rowIndex % 2 !== 0,
+              'rounded bg-16591D text-white p-3 width-3': rowIndex % 2 === 0
+            }">
               {{ hora[index] }}
             </div>
           </div>
         </div>
       </div>
-    </div>
-
-    <div class="flex relative mt-3">
-      <div class="flex-1"></div>
-      <div class="justify-end">
-        <Button label="Modificar Materias" icon="pi pi-pencil" severity="info" size="large" class="ml-5 bg-0D294C" @click="openDialog2" />
-        <Button label="Modificar Horario" icon="pi pi-pencil" severity="info" size="large" class="ml-5 bg-0D294C" @click="openDialog" />
-      </div>
-    </div>
+    </div> -->
 
   </div>
 
   <!-- Dialogos -->
 
-  <Dialog v-model="showDialog" header="Horario" :visible="showDialog" 
-    class="text-0D294C text-4xl ml-2.4%" style="width: 75%">
+  <Dialog v-model:visible="showDialogHorarios" modal header="Horario" style="width: 75%">
 
     <div class="flex flex-wrap">
       <div v-for="(dia, index) in ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes']" :key="index" class="p-row">
         <!-- Horas -->
         <div v-for="(hora, rowIndex) in horario" :key="rowIndex" class="width-3 text-3xl p-5">
-          <div
-            :class="{
-              'rounded bg-E2F4E4 text-black p-3 width-3': rowIndex % 2 !== 0,
-              'rounded bg-16591D text-white p-3 width-3': rowIndex % 2 === 0
-            }"
-          >
+          <div :class="{
+            'border-round-md bg-E2F4E4 text-black p-3 width-3': rowIndex % 2 !== 0,
+            'border-round-md bg-16591D text-white p-3 width-3': rowIndex % 2 === 0
+          }">
             {{ hora[index] }}
           </div>
         </div>
@@ -149,19 +134,19 @@ const closeDialog2 = () => {
     </div>
 
     <div style="display: flex; flex-direction: column; align-items: flex-start;">
-      <div v-for="(dia, index) in ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes']" :key="index" class="p-row flex items-center">
+      <div v-for="(dia, index) in ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes']" :key="index"
+        class="p-row flex items-center">
         <div style="display: grid; grid-template-columns: repeat(4, 1fr);">
 
           <div class="width-3 text-3xl p-2 font-bold m-4">{{ dia }}</div>
 
-          <input class="width-3 p-4 m-4 font-semibold bg-0D294C text-3xl rounded" placeholder="Hora de Inicio"/>
+          <input class="width-3 p-4 m-4 font-semibold bg-0D294C text-3xl rounded" placeholder="Hora de Inicio" />
 
-          <input style="display: flex; gap: 5px; align-items: center;" 
-            class="width-3 p-4 m-4 font-semibold bg-0D294C text-3xl rounded"
-          placeholder="Hora de Fin"/>
+          <input style="display: flex; gap: 5px; align-items: center;"
+            class="width-3 p-4 m-4 font-semibold bg-0D294C text-3xl rounded" placeholder="Hora de Fin" />
 
           <div class="w-1 text-3xl m-4">
-            <i class="pi pi-plus" ></i>
+            <i class="pi pi-plus"></i>
           </div>
         </div>
       </div>
@@ -169,22 +154,19 @@ const closeDialog2 = () => {
     <div class="flex justify-end mt-2">
       <div class="flex-1"></div>
       <div class="justify-end">
-        <Button label="Listo" icon="pi pi-check" @click="closeDialog"
-        severity="info" size="large" class="ml-5 bg-0D294C"/>
+        <Button label="Listo" icon="pi pi-check" @click="closeDialog" severity="info" size="large"
+          class="ml-5 bg-0D294C" />
       </div>
     </div>
   </Dialog>
 
-  <Dialog v-model="showDialog2" header="Materias" :visible="showDialog2" class="text-0D294C text-4xl ml-1.4%" 
-  style="width: 75%">
+  <Dialog v-model:visible="showDialogMaterias" modal header="Materias" style="width: 75%">
     <div class="flex flex-col">
       <!-- Etiquetas de materias -->
       <div class="flex flex-wrap">
-        <div v-for="(materia, index) in filteredMaterias.slice(0, showMoreTags2 ? Infinity : 8)" :key="index" class="w-3 h-3 p-4">
-          <Tag
-            rounded
-            class="w-full h-4rem text-lg font-semibold bg-0D294C text-white"
-          >
+        <div v-for="(materia, index) in filteredMaterias.slice(0, showMoreTags2 ? Infinity : 8)" :key="index"
+          class="w-3 h-3 p-4">
+          <Tag rounded class="w-full h-4rem text-lg font-semibold bg-0D294C text-white">
             {{ materia }}
           </Tag>
         </div>
@@ -194,18 +176,14 @@ const closeDialog2 = () => {
           <div class="flex relative mt-3">
             <div class="flex-1"></div>
             <div class="justify-end">
-              <Button
-                :class="{'pi pi-plus-circle': !showMoreTags2, 'pi pi-minus-circle': showMoreTags2}" 
-                size="large" class="ml-5 bg-0D294C"
-                label="Etiquetas"
-              />
+              <Button :class="{ 'pi pi-plus-circle': !showMoreTags2, 'pi pi-minus-circle': showMoreTags2 }" size="large"
+                class="ml-5 bg-0D294C" label="Etiquetas" />
             </div>
           </div>
         </div>
 
         <!-- Barra de búsqueda de materias -->
-        <InputText v-model="searchQuery" placeholder="Buscar materia" class="p-mr-2 w-11 mb-6 mt-6" 
-          style="font-size: 1.5rem; background-color: #c2c2c2; color: #000000;::placeholder 
+        <InputText v-model="searchQuery" placeholder="Buscar materia" class="p-mr-2 w-11 mb-6 mt-6" style="font-size: 1.5rem; background-color: #c2c2c2; color: #000000;::placeholder 
           { color: black; }" />
 
         <!-- Componente a la izquierda -->
@@ -215,10 +193,7 @@ const closeDialog2 = () => {
 
         <!-- Componente al centro -->
         <div class="width-30">
-          <Tag
-            rounded
-            class="h-4rem text-lg font-semibold bg-0D294C text-white"
-          >
+          <Tag rounded class="h-4rem text-lg font-semibold bg-0D294C text-white">
             Ejemplo
           </Tag>
         </div>
@@ -235,100 +210,71 @@ const closeDialog2 = () => {
     <div class="flex justify-end mt-2">
       <div class="flex-1"></div>
       <div class="justify-end">
-        <Button label="Listo" icon="pi pi-check" @click="closeDialog2" severity="info" size="large" class="ml-5 bg-0D294C"/>
+        <Button label="Listo" icon="pi pi-check" @click="closeDialog2" severity="info" size="large"
+          class="ml-5 bg-0D294C" />
       </div>
     </div>
   </Dialog>
-
 </template>
 
 <style scoped>
-  /* Estilo de colores */
-  .text-0D294C {
-    color: #0D294C;
-    font-weight: bold;
-  }
 
-  .bg-FF69B4{
-      background-color: #FF69B4
-  }
-  /* Estilo del botón */
-  .bg-0D294C {
-    background-color: #0D294C;
-    color: #fff;
-    border: 1px solid #0D294C;
-    border-radius: 20px;
-  }
+/* Horizontal spacing utility class */
+.space-x > * {
+  margin-right: 1rem;
+}
 
-  /* Línea gris */
-  .border-D6D6D6 {
-    border-color: #D6D6D6;
-  }
+/* Vertical spacing utility class */
+.space-y > * {
+  margin-bottom: 1rem; 
+}
 
-  /* Establece el tamaño fijo de la línea horizontal */
+
+.bg-0D294C {
+  background-color: #0D294C;
+  color: #fff;
+  border: 1px solid #0D294C;
+  border-radius: 20px;
+}
+
+@media (max-width: 768px) {
+  .width-3 {
+    width: 100%;
+  }
+}
+
+@media (max-width: 768px) {
+  .width-2 {
+    width: 25%;
+  }
+}
+
+@media (max-width: 768px) {
+  .widthDialog-3 {
+    width: 37.5%;
+  }
+}
+
+
+@media (max-width: 768px) {
   .width-5 {
-    width: 41.6667%;
+    width: 55%;
   }
-  .width-2{
-      width: 16.6667%;
-  }
+}
 
+/* Redondear las horas */
+.rounded {
+  border-radius: 20px;
+}
+.width-media {
+  width: 50%;
+}
 
-  .widthDialog-3{
-      width: 25%;
-  }
+.width-30 {
+  width: 30%;
+}
 
-
-  @media (max-width: 768px) {
-    .width-3 {
-      width: 100%;
-    }
-  }
-
-  @media (max-width: 768px) {
-    .width-2 {
-      width: 25%; 
-    }
-  }
-  @media (max-width: 768px) {
-    .widthDialog-3 {
-      width: 37.5%; 
-    }
-  }
-
-
-  @media (max-width: 768px) {
-    .width-5 {
-      width: 55%; 
-    }
-  }
-
-  /* Redondear las horas */
-  .rounded {
-    border-radius: 20px;
-  }
-
-  /* Estilo de colores */
-  .bg-F4F4F4 {
-    background-color: #d8d6d6;
-  }
-
-  .bg-E2F4E4 {
-    background-color: #a5eba5;
-    color: #000;
-  }
-
-  .bg-16591D {
-    background-color: #16591D;
-    color: #fff;
-  }
-  .width-media{
-    width: 50%; 
-  }
-  .width-30{
-    width: 30%; 
-  }
-  .width-20{
-    width: 20%; 
-  }
+.width-20 {
+  width: 20%;
+}
 </style>

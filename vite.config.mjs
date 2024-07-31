b00460a670ead02ebb59e70d7f7ff166d06f601e
Cagatay Civici <cagatay.civici@gmail.com>
2024-07-31 10:57:07 +0300

    Disable optimizer

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,15 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
-import { PrimeVueResolver } from '@primevue/auto-import-resolver';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    optimizeDeps: {
+        noDiscovery: true
+    },
     plugins: [
         vue(),
         Components({

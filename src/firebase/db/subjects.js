c0639a3db35237eb0b510ec5ce8c374e38d663f8
Luis Ángel Guzmán Iribe <luisangel2002.lagi@gmail.com>
2024-08-04 14:21:02 -0400

    hotfix: Las materias se reciben en orden alfabetico por el nombre de la materia

--- src/firebase/db/subjects.js
+++ src/firebase/db/subjects.js
@@ -1,15 +1,19 @@
 import { firestoreDB } from "../../main";
 import {
     collection,
-    getDocs
+    getDocs,
+    query,
+    orderBy,
 } from 'firebase/firestore';
 
 export async function getSubjects() {
     const subjectsRef = collection(firestoreDB, "schools/tec.mx/subjects");
+    
+    const q = query(subjectsRef, orderBy("name"));
 
-    const docsSnap = await getDocs(subjectsRef);
+    const docsSnap = await getDocs(q);
 
-    if (docsSnap) {
+    if (!docsSnap.empty) {
         return docsSnap.docs.map(doc => doc.data());
     } else {
         return null;

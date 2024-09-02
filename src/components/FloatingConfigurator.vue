411fecb51743212f3490eb29d3481739b267ef30
Cagatay Civici <cagatay.civici@gmail.com>
2024-09-02 15:59:14 +0300

    Fixed warning

--- src/components/FloatingConfigurator.vue
+++ src/components/FloatingConfigurator.vue
@@ -7,7 +7,7 @@ const { toggleDarkMode, isDarkTheme } = useLayout();
 
 <template>
     <div class="fixed flex gap-4 top-8 right-8">
-        <Button type="button" @click="toggleDarkMode" rounded :icon="{ 'pi pi-moon': isDarkTheme, 'pi pi-sun': !isDarkTheme }" severity="secondary" />
+        <Button type="button" @click="toggleDarkMode" rounded :icon="isDarkTheme ? 'pi pi-moon' : 'pi pi-sun'" severity="secondary" />
         <div class="relative">
             <Button
                 icon="pi pi-palette"

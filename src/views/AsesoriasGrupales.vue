83a6dbc8492c9df2d2ee3c35c9d4f9995c88d017
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2024-10-16 10:19:43 -0600

    Add filters

--- src/views/AsesoriasGrupales.vue
+++ src/views/AsesoriasGrupales.vue
@@ -1,48 +1,82 @@
 <script setup>
-    import { onMounted, ref } from 'vue';
-    import { getAnnouncementsGrupales } from '@/firebase/db/annoucement'; 
-    import { getSubjectColor } from '@/utils/HorarioUtils';
-    import {
-         formatDate,
-         formatTime
-    } from '@/utils/AnunciosUtils';
+import { onMounted, ref, computed } from 'vue';
+import { getAnnouncementsGrupales } from '@/firebase/db/annoucement'; 
+import { getSubjects } from '../firebase/db/subjects';
+import { getSubjectColor } from '@/utils/HorarioUtils';
+import { formatDate, formatTime } from '@/utils/AnunciosUtils';
 
+const asesorias = ref([]);
+const subjects = ref([]);
+const subjectsFilter = ref(null);  // Filtro de materias
 
-    const asesorias= ref([]);
-    onMounted(async () => {
+onMounted(async () => {
+    subjects.value = await getSubjects();
+    asesorias.value = await getAnnouncementsGrupales();
+});
 
-        asesorias.value = await getAnnouncementsGrupales();
-    });
+// Filtrar asesorías según la materia seleccionada
+const filteredAsesorias = computed(() => {
+    const selectedSubject = subjectsFilter.value;
+    
+    if (selectedSubject === null) {
+        return asesorias.value;  // Mostrar todas si no hay filtro
+    }
 
+    return asesorias.value.filter(asesoria => 
+        asesoria.subject.id === selectedSubject.id
+    );
+});
 
-
+const clearFilters = () => {
+    subjectsFilter.value = null;
+};
 </script>
+
 <template>
-    <div>
-        <h1 class="text-black text-6xl font-bold mb-5 text-center sm:text-left">Asesorías Grupales</h1>
+    <div class="flex flex-column md:flex-row md:items-center justify-content-between mb-3"> 
+        <div>
+            <h1 class="text-black text-6xl font-bold mb-5 text-center sm:text-left">Asesorías Grupales</h1>
+        </div>
+        <span class="w-full md:w-5 mt-2 justify-content-end"> 
+            <Dropdown v-model="subjectsFilter" 
+            :options="subjects" editable optionLabel="name" 
+            placeholder="Materia" checkmark :highlightOnSelect="false"  
+            class="w-full p-0 border-gray-300 rounded-lg shadow-md focus:ring-2 focus:ring-blue-500 transition duration-300 "  />
+        </span>
+    </div>
+
+    <div v-if="subjectsFilter && filteredAsesorias.length === 0" class="flex align-content-center h-full" style="min-height: 400px">
+        <h1 class="flex align-items-center justify-content-center w-full text-center mt-8">
+            <span>Buscando MAEs por materia... 🔎</span>
+        </h1>
+    </div>
+
+    <div v-if="asesorias.length === 0 && !subjectsFilter" class="flex align-content-center h-full" style="min-height: 300px">
+        <h1 class="flex align-items-center justify-content-center w-full text-center">
+            <span>No hay asesorías grupales de momento 😔 <br> Consulta los <router-link to="horarios">horarios</router-link> para saber más ❤️</span>
+        </h1>
     </div>
 
     <div class="flex flex-wrap gap-4">
         <div 
-            v-for="asesoria in asesorias" 
+            v-for="asesoria in filteredAsesorias" 
             :key="asesoria.id" 
             class="flex flex-col md:flex-row bg-white border-round-3xl w-full md:w-5 boder-gray card-container"
             style="height: 190px;"
-           
         >   
             <div 
                 class="color-bar" 
                 :class="getSubjectColor(asesoria.subject.area)"
             ></div>
 
-            <div class="px-5  mt-4 w-full">
+            <div class="px-5 mt-4 w-full">
                 <div style="height: 50px">
-                    <p class="font-bold text-lg  text-center">
+                    <p class="font-bold text-lg text-center">
                         {{ asesoria.subject.name }}
                     </p>
                 </div>
 
-                <div class="flex align-items-center ">
+                <div class="flex align-items-center">
                     <img src="/assets/ubicacion.svg" class="mr-2" alt="ubicacion icon" style="width: 1.4rem; height: 1.4rem;" />
                     <p>{{ asesoria.location }}</p>
                 </div>
@@ -55,26 +89,23 @@
                         {{ formatTime(asesoria.endTime, true) }}
                     </p>
                 </div>
-                
+
                 <div class="flex justify-content-end mt-2">
-                <Button 
-                    label="Pre-registro" 
-                    class="custom-button font-bold text-black mt-2 text-md btn border-round-xl flex align-items-center" 
-                    @click="handleSubmit">
-                    <span>Pre-registro</span>
-                    <i class="pi pi-arrow-right text-md ml-2 font-bold text-white"></i>
-                </Button>
+                    <Button 
+                        label="Pre-registro" 
+                        class="custom-button font-bold text-black mt-2 text-md btn border-round-xl flex align-items-center" 
+                        @click="handleSubmit"
+                    >
+                        <span>Pre-registro</span>
+                        <i class="pi pi-arrow-right text-md ml-2 font-bold text-white"></i>
+                    </Button>
                 </div>
-
-
             </div>
         </div>
     </div>
 </template>
 
-
 <style>
-
 .card-container {
     position: relative;
     overflow: hidden;
@@ -89,7 +120,7 @@
     border-radius: 1.5rem 1.5rem 0 0;
 }
 
-.boder-gray{
+.boder-gray {
     border: 2px solid #e0e0e0;
 }
 
@@ -97,5 +128,4 @@
     color: white; 
     background: linear-gradient(to right, #4466A7, #51A3AC);
 }
-
 </style>

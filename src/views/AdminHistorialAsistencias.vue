ac2322b01ca7e02afac7fd3b33076574eadd422e
Monica Guzman <a00838824@tec.mx>
2025-03-31 10:07:18 -0600

    Hice la página de historial y agregué el route a la página

---
+++ src/views/AdminHistorialAsistencias.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="sm:flex sm:justify-content-between mb-2 sm:mb-5">
+        <h1 class="text-black text-6xl font-bold text-center m-0 sm:text-left">Historial de asistencia</h1>
+    </div>
+</template>

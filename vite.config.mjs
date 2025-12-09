7c32ae1f6fc75e18a5e735f76e1ab25d1167d26c
tugcekucukoglu <tugcekucukoglu.860@gmail.com>
2025-12-09 14:05:01 +0300

    chore: remove sass warnings

--- vite.config.mjs
+++ vite.config.mjs
@@ -22,5 +22,12 @@ export default defineConfig({
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler'
+            }
+        }
     }
 });
